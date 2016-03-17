46c7d16910671db25dfc3b5e56d9183a6b9b711b
Teemu Virenius <k2000@student.jamk.fi>
2016-03-17 16:00:44 +0200

    Tein Car-luokan(ominaisuudet) sekä Cars luokan(List-Car) sekä loin peliosuuden näyttöosuuden.

---
+++ Kiihdytyspeli/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiihdytyspeli
+{
+    class Car
+    {
+        // field variables -- Sisäiset muuttujat
+        private int speed;
+        private int gearAmount;
+
+        // property variables -- Ominaisuudet
+        public int Speed { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public int MaxSpeed { get; set; }
+        public int CurrentGear { get; set; }
+        public int GearAmount { get; set; }
+        public int CarPrice { get; set; }
+
+        // methods -- Toiminnallisuudet
+
+            // Suurempi vaihde
+        public void GearShiftUp()
+        {
+            CurrentGear += 1;
+        }
+
+        // Pienempi vaihde
+        public void GearShiftDown()
+        {
+            CurrentGear -= 1;
+        }
+
+        // Kiihdytys
+        public void Accelerate()
+        {
+            Speed += 20;
+        }
+        
+        // Jarrutus
+        public void Brake()
+        {
+            Speed -= 20;
+        }
+
+        public override string ToString()
+        {
+            return Speed + " " + Color + Brand + " " + MaxSpeed + CurrentGear + " " + GearAmount + "" + CarPrice;
+        }
+    }
+}
---
+++ Kiihdytyspeli/Cars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiihdytyspeli
+{
+    class Cars: Car
+    {
+        // field variables
+        private List<Car> cars;
+
+        // property variables
+        public string Owner { get; set; }
+        public Cars()
+        {
+            cars = new List<Car>();
+        }
+
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+
+        public void PrintData()
+        {
+            foreach (Car car in cars)
+            {
+                car.ToString();
+            }
+        }
+
+    }
+}
